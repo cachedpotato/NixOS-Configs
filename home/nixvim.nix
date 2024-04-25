e490b9f6e52edbdeb32257fcd72c6c9d977b601f
cachedpotato <chiwonchung98@gmail.com>
2024-04-25 20:12:37 +0900

    nixvim

---
+++ home/nixvim.nix
@@ -0,0 +1,75 @@
+{pkgs, config, inputs, ...}:
+{
+  imports = [
+    inputs.nixvim.homeManagerModules.nixvim
+  ];
+
+  programs.nixvim = {
+    enable = true;
+
+    plugins = {
+      lualine.enable = true;
+      nvim-autopairs.enable = true;
+      indent-blankline = {
+        enable = true;
+        settings.indent.char = "";
+      };
+    };
+
+    opts = {
+      #tabs
+      tabstop = 2;
+      expandtab = true;
+      shiftwidth = 2;
+
+      #indents
+      autoindent = true;
+      smartindent = true;
+
+      #line number
+      number = true;
+      relativenumber = true;
+
+      #Misc
+      wrap = false;
+      guicursor = "a:block-CursorLineNr";
+      colorcolumn = "100";
+      ignorecase = true;
+      smartcase = true;
+    };
+
+    globals.mapleader = " ";
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>h";
+        action = ":noh<CR>";
+        options = {
+          silent = true;
+          noremap = true;
+          desc = "remove [h]ighlight";
+        };
+      }
+      {
+        mode = "n";
+        key = "<Alt-u>";
+        action = "<C-u>zz";
+        options = {
+          silent = true;
+          noremap = true;
+          desc = "move [u]p half screen and adjust cursor";
+        };
+      }
+      {
+        mode = "n";
+        key = "<Alt-d>";
+        action = "<C-d>zz";
+        options = {
+          silent = true;
+          noremap = true;
+          desc = "move [d]own half screen and adjust cursor";
+        };
+      }
+    ];
+  };
+}
