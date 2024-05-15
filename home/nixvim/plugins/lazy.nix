47b66840953c5fdce99d17fd6a1bd451c6e1bd83
cachedpotato <chiwonchung98@gmail.com>
2024-05-15 11:38:11 +0900

    you know what I can live with 300ms startup

--- home/nixvim/plugins/lazy.nix
+++ home/nixvim/plugins/lazy.nix
@@ -1,22 +1,42 @@
+{pkgs, ...}:
 {
   #BufReadPre = Open Preexisting Files
   #BufNewFile = Open New File
   #InsertEnter = When we go into Insert Mode
   programs.nixvim.plugins.lazy = {
     enable = true;
-    plugins = [
+    plugins = with pkgs.vimPlugins; [
       {
-        name = "gitsigns";
-        dev = true;
+        pkg = gitsigns-nvim;
+        dir = "/nix/store/aqfirl32akxm1zfmkg9zx11mb26vqifp-vimplugin-lua5.1-gitsigns.nvim-scm-1-unstable-2024-05-04/lua/gitsigns";
         event = ["BufReadPre" "BufNewFile"];
       }
+      {
+        pkg = nvim-lspconfig;
+        dir = "/nix/store/gmzfpq146r13iipky26131zfz64fjm6h-vimplugin-nvim-lspconfig-2024-05-06/lua/lspconfig";
+        event = ["BufReadPre" "BufNewFile"];
+      }
+      {
+        pkg = indent-blankline-nvim;
+        event = ["BufReadPre" "BufNewFile"];
+      }
+      {
+        pkg = nvim-autopairs;
+        event = ["InsertEnter"];
+      }
+      {
+        pkg = nvim-cmp;
+        event = ["InsertEnter"];
+      }
+      {
+        pkg = nvim-treesitter;
+        event = ["BufReadPre" "BufNewFile"];
+      }
+      {
+        pkg = obsidian-nvim;
+        event = ["BufReadPre" "BufNewFile"];
+        ft = "md";
+      }
     ];
-
-    #gitsigns.event = ["BufReadPre" "BufNewFile"];
-    #nvim-autopairs.event = ["InsertEnter"];
-    #indent-blankline.event = ["BufReadPre" "BufNewFile"];
-    #lsp.event = ["BufReadPre" "BufNewFile"];
-    #cmp.event = ["InsertEnter"];
-    #treesitter.event = ["BufReadPre" "BufNewFile"];
   };
 }
